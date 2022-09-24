78196a6f64345bc339c51bbed2c1f9227dc98edc
shivaiitb21 <shivnalgire4@gmail.com>
2022-09-24 15:51:08 +0530

    map filter

--- lambdaf.py
+++ lambdaf.py
@@ -1,3 +1,15 @@
+# Lambda function
 b = lambda x: "Even" if x%2==0 else "Odd"
-
 print(b(5))
+
+print()
+
+# map function
+l = [1,2,3]
+print(list(map(lambda x:x*2, l)))
+
+print()
+
+#Filter function
+print(list(filter(lambda x: x>2, l)))
+
