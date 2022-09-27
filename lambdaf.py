d273e801b115b7f6baf169343b5d3ec3c9589809
shivaiitb21 <shivnalgire4@gmail.com>
2022-09-27 18:24:32 +0530

    lambda

--- lambdaf.py
+++ lambdaf.py
@@ -1,15 +1,26 @@
-# Lambda function
-b = lambda x: "Even" if x%2==0 else "Odd"
-print(b(5))
+# # Lambda function
+# from sys import modules
 
-print()
 
-# map function
-l = [1,2,3]
-print(list(map(lambda x:x*2, l)))
+# b = lambda x: "Even" if x%2==0 else "Odd"
+# print(b(5))
 
-print()
+# print()
 
-#Filter function
-print(list(filter(lambda x: x>2, l)))
+# # map function
+# l = [1,2,3]
+# print(list(map(lambda x:x*2, l)))
 
+# print()
+
+# #Filter function
+# print(list(filter(lambda x: x>2, l)))
+
+# help('modules')
+
+fruits = ["mango", "apple"]
+veg = ["spinach", "kale"]
+
+zone = [fruits, veg]
+
+print(zone[1][1])
