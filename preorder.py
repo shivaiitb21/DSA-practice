a963edc01b5f1b7b60937200d2aab1331ad465dc
shivaiitb21 <shivnalgire4@gmail.com>
2022-10-11 16:29:07 +0530

    binary tree post order traversal

---
+++ preorder.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+ 
+# A function to do postorder tree traversal
+def printPostorder(root):
+ 
+    if root:
+ 
+        # First recur on left child
+        printPostorder(root.left)
+ 
+        # the recur on right child
+        printPostorder(root.right)
+ 
+        # now print the data of node
+        print(root.val)
